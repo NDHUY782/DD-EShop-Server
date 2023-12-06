d92a5822408460ddb5a62895524ae676c1cbcd5f
PhucNguyenHoang <hoangphucnguyen112002@gmail.com>
2023-12-06 14:13:35 +0700

    First commit

---
+++ src/models/authen_model.js
@@ -0,0 +1,55 @@
+const bcrypt = require("bcrypt")
+const { Schema , model } = require("mongoose")
+
+const AuthenModel = new Schema({
+    username: {
+        type : String,
+        required: true,
+    },
+    status : {
+        type : String,
+    },
+    password: {
+        type : String,
+        required: true,
+    },
+    avatar: {
+        type : String,
+        required: true,
+    },  
+    hoten: {
+        type : String,
+    },
+    phone: {
+        required: true,
+        type : String,
+    },
+    email: {
+        type : String,
+        required: true,
+    },
+    role: {
+        type : String,
+    },
+    boss: {
+        type : String,
+    },
+    lastDateLogin: {
+        type : Date,
+    }
+    
+}, {
+    timestamps : true
+}) 
+
+// AuthenModel.pre("save", async function (next) {
+//     if (!this.isModified("password")) {
+//         return next
+//     }
+//     let hash = await bcrypt.genSalt(10).then((salt => bcrypt.hash(this.password, salt)))
+//     this.password = hash
+//     next();
+// })
+
+
+module.exports = model('authen' , AuthenModel)
