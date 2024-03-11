2e5f51663015444ded09cd3fb2be0fc765074b60
Duc Huy <huynguyen07080401@gmail.com>
2024-03-11 21:39:34 +0700

    add quantity cart

--- src/controllers/backend/cart_controller.js
+++ src/controllers/backend/cart_controller.js
@@ -48,23 +48,17 @@ const add_to_cart = async(req,res) =>{
             } else {
                 product_price= productData.price
             }
-            const quantity = req.body.quantity
+            const quantity = parseInt(req.body.quantity)
             const cartData = await CartModel.findOne({user_id: user_id, product_id: productID})
             if (cartData) {
-                if (quantity == null) {
-                    await cartData.updateOne({ 
-                        $set: {
-                            quantity: cartData.quantity + 1
-                        }
-                })
-                } else {
+
                     await cartData.updateOne({ 
                         $set: {
                             quantity: cartData.quantity + quantity
                         }
                 })
-                }
                 res.status(202).send({success: true,msg:'thành công'})
+
                 // if (cartData.user_id === userData._id && cartData.product_id === productData._id) {
                 //     const  sessionUser= userData._id
                 //     const  productId = productData._id
@@ -88,7 +82,6 @@ const add_to_cart = async(req,res) =>{
                     product_image:   productData.avatar,
                     product_price:   product_price,
                     product_name :   productData.name,
-                    quantity     :   req.body.quantity
                 })
                 cart_obj.save()
                 res.status(202).send({success: true,msg:'thành công',data:cart_obj})
